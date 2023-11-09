26499c1695fc6ce0e01cbad1761ae6121ebae4cf
Vasco Saraiva <vasco.saraiva@ua.pt>
2023-11-09 09:23:24 +0000

    Updated some parts of the exercise

--- ex01-react/app/layout.tsx
+++ ex01-react/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { cookies } from 'next/headers';
-import { ClientCookiesProvider } from '@/util/CookiesProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +17,7 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <ClientCookiesProvider value={cookies().getAll()}>
         <body className={inter.className}>{children}</body>
-      </ClientCookiesProvider>
     </html>
   )
 }
--- ex01-react/components/TodoFilter.tsx
+++ ex01-react/components/TodoFilter.tsx
@@ -30,13 +30,14 @@ const TodoFilter = ({tasks, setFilteredTasks} : TodoFilterProps) => {
 
   return (
     <div className="flex gap-2 mt-5">
-      {filters.map((filter, index) => (
+      {filters.map((element, index) => (
 
         <button
-          key={filter + index}
-          className="border-2 rounded-md border-gray-300 w-28 py-2 hover:bg-gray-100 transition-colors duration-200"
+          key={element + index}
+          className={`border-2 rounded-md border-gray-300 w-28 py-2 hover:bg-gray-100 transition-colors duration-200
+          ${element === filter && 'bg-gray-100'}`}
           onClick={e => e.currentTarget.textContent && setFilter(e.currentTarget.textContent)}>
-          {filter}
+          {element}
         </button>
 
       ))}
